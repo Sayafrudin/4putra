492e238f3db836ab9391c5c263683d1a72809950
Syafrudin Hendra Lumanto <syafrudintdm@gmail.com>
2026-01-03 02:38:14 +0700

    add icon & fixing image

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -5,6 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>4Putra Vertex Aviary</title>
+    <link rel="icon" href="img/4Putraico.png" type="image/png">
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 
